1ff26db4ef5e6bffbf4b808b5f6f573e1965b5b7
Ilmiddin O'rinov <ilmiddinorinov1@gmail.com>
2024-05-04 14:47:32 +0500

    to do app

--- app/src/main/java/com/example/todoapp/Utils/obj.kt
+++ app/src/main/java/com/example/todoapp/Utils/obj.kt
@@ -4,20 +4,18 @@ import com.example.todoapp.Models.Spinner
 import com.example.todoapp.R
 
 object obj {
-    val images =
-        intArrayOf(
-            R.drawable.red_flag,
-            R.drawable.yellow_flag,
-            R.drawable.blue_flag,
-            R.drawable.gray_flag
-        )
-    val degrees =
-        arrayOf(
-            "Urgent",
-            "High",
-            "Normal",
-            "Low"
-        )
+    val images = intArrayOf(
+        R.drawable.red_flag,
+        R.drawable.yellow_flag,
+        R.drawable.blue_flag,
+        R.drawable.gray_flag
+    )
+    val degrees = arrayOf(
+        "Urgent",
+        "High",
+        "Normal",
+        "Low"
+    )
     var list: ArrayList<Spinner>? = null
         get() {
             if (field != null) {
